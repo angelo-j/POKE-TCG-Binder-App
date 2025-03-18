6f661397d1f9b742037c4cd006828830bc3c13f4
Jordan Opst <opstja@gmail>
2025-03-18 03:54:03 -0400

    Test commit.

--- src/main/java/com/pokebinderapp/Application.java
+++ src/main/java/com/pokebinderapp/Application.java
@@ -14,6 +14,7 @@ import java.util.Base64;
 public class Application {
 
     // todo: fix tests
+    // todo: allow user to input quantity during trades
 
   public static void main(String[] args) {
       SpringApplication.run(Application.class, args);
