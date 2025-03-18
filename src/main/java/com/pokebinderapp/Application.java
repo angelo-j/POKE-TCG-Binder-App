f9af1d2f559d13c8109753bbdfcf89bb35ffbb35
Jordan Opst <opstja@gmail>
2025-03-18 03:18:58 -0400

    Fixed startup.

--- src/main/java/com/pokebinderapp/Application.java
+++ src/main/java/com/pokebinderapp/Application.java
@@ -15,14 +15,10 @@ public class Application {
 
     // todo: fix tests
 
-//   public static void main(String[] args) {
-//       SpringApplication.run(Application.class, args);
-//   }
+  public static void main(String[] args) {
+      SpringApplication.run(Application.class, args);
+  }
 
-    public static void main(String[] args) {
-        SecretKey key = Keys.secretKeyFor(io.jsonwebtoken.SignatureAlgorithm.HS512);
-        String base64Key = Base64.getEncoder().encodeToString(key.getEncoded());
-        System.out.println("Generated JWT Secret Key: " + base64Key);
-    }
+
 
 }
